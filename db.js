983bfff8426c0c831f77c4da61cfb5814412c8a4
vineeth1607 <vineethayaan@gmail.com>
2024-03-15 18:41:32 +0530

    connected to express and mongo

---
+++ db.js
@@ -0,0 +1,14 @@
+const { default: mongoose } = require('mongoose');
+
+const mongoURI = "mongodb://localhost:27017/";
+
+const connectToMongo = async () => {
+    try {
+        await mongoose.connect(mongoURI);
+        console.log("Connected to MongoDB successfully");
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error.message);
+    }
+};
+
+module.exports = connectToMongo;
